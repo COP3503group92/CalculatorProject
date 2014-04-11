f1fba5fac4bb79eed08204a63e0c7181d8438b9f
kinderleycharles <kinderleycharles@aol.com>
2014-04-11 17:34:37 -0400

    Menu/ShuntingYard
    This is all the sutff that I have so far

---
+++ CalculatorProject/CalculatorProject/ShuntingYard.cpp
@@ -0,0 +1,537 @@
+#include "ShuntingYard.h"
+#include <iostream>
+#include <stdexcept>
+#include <string>
+#include <vector>
+#include <sstream>
+
+using namespace std;
+
+ShuntingYard::ShuntingYard(void)
+{
+	string s = "";
+}
+
+ShuntingYard::~ShuntingYard(void)
+{
+}
+
+// SET METHODS
+void ShuntingYard::setString(string input)
+{
+	s = input;
+}
+void ShuntingYard::setQueue(vector<string> input)
+{
+	queue = input;
+}
+
+// GET METHODS
+string ShuntingYard::getS()
+{
+	return s;
+}
+vector<string> ShuntingYard::getQueue()
+{
+	return queue;
+}
+
+void ShuntingYard::removeSpaces(){
+    //creates a new expression string without any spaces
+    string out;
+    int i = 0;
+    while (i < s.length()){
+		char a = s.at(i);
+        if (a != ' '){
+			out = out + a;
+        }
+        i++;
+    }
+	
+	cout << "Remove Space Check\n" << out << endl << endl;
+	s = out;
+}
+
+// Method:		isNumber(char in)
+// Description:	Take an input and check to see if it is
+//				a digit. Return true if true else return false
+// Return		true or false
+bool ShuntingYard::isNumber(char in){
+    //if statement checks if value is a number
+    int character = (int)in;
+    if ( (character >= 48) && (character <= 57) ){
+        return true;
+    }
+    else {
+        return false;
+    }
+}
+
+// Method:		isNumber(char in)
+// Description:	Take an input and check to see if it is
+//				a digit. Return true if true else return false
+// Return		true or false
+bool ShuntingYard::isSpecialNumber(string in){
+    //if statement checks if input is pi or e
+    if ( (in == "PI") || (in == "Pi") || (in == "pI") || (in == "pi")){
+        return true;
+    }
+	else if ((in == "e")) {
+		return true;
+	}
+    else {
+        return false;
+    }
+}
+
+// Method:		isOperator(char)
+// Parameter:	char op
+// Desrciption:	Check if the chararacter is one of the assigned character +,-,*,/
+//				return true if it is and false if not
+// return:		bool	
+bool ShuntingYard::isOperator(char op)
+{
+	if((op == '+') || (op == '-') || (op == '*') || 
+	   (op == '^') || (op == '/') ) {
+		
+		return true;
+	}
+	else {
+		return false;
+	}
+}
+bool ShuntingYard::isOperator(string op)
+{
+	if((op == "+") || (op == "-") || (op == "*") || 
+	   (op == "^") || (op == "/") ) {
+		
+		return true;
+	}
+	else {
+		return false;
+	}
+}
+bool ShuntingYard::isOpenParen(char in){
+    //returns true if the char in is a '('
+    int character = (int)in;
+    if (character == 40) {
+        return true;
+    }
+    else {
+        return false;
+    }
+}
+bool ShuntingYard::isOpenParen(string in){
+    //returns true if the char in is a '('
+    if (in == "(") {
+        return true;
+    }
+    else {
+        return false;
+    }
+}
+bool ShuntingYard::isCloseParen(char in){
+    //returns true if the char in is a ')'
+    int character = (int)in;
+    if (character == 41) {
+        return true;
+    }
+    else {
+        return false;
+    }
+}
+bool ShuntingYard::isCloseParen(string in){
+    //returns true if the char in is a ')'
+    if (in == ")") {
+        return true;
+    }
+    else {
+        return false;
+    }
+}
+
+// Method:		operatorToString(char)
+// Parameter:	char op
+// Description:	This method converts the operation sign
+//				to string types so that it can be stored in the string vector
+std::string ShuntingYard::operatorToString(char op)
+{
+	string temp;
+	temp.push_back(op);
+	return temp;
+}
+
+// Method:		precedence(string)
+// parameters:	string operation
+// Description:	This method takes in a string which should be a character (+,-,*,/)
+//				except in the case that it is log_. It returns a number corresponding
+//				to the operation string.
+// Return:		int a number
+int ShuntingYard::precedence(std::string operation)
+{
+	if ((operation == "^")) {
+		return 2;
+	}
+	else if((operation == "/") || (operation == "*") ) {
+		return 1;
+	}
+	else if((operation == "+") || (operation == "-") ) {
+		return 0;
+	}
+	else {
+		// For now return -1 if you do not know what it is
+		// This is just in case log_, e, sqrt, and rt
+		return -1;
+	}
+}
+
+// Method:		addMissingOperator(string)
+void ShuntingYard::addMissingOPerator()
+{
+	string out = "";
+	for(int i = 0; i < s.length(); i++) {
+		// For cases like this -(9)
+		// Change to (-1)*(9)
+		if((i == 0) && (i + 1 < s.length()) && (s.at(i) == '-') && 
+			((isNumber(s.at(i+1))) || (s.at(i+1) == 'e') || (s.at(i+1) == 'p')
+			|| (isOpenParen(s.at(i+1)))) ) {
+				out.push_back('(');
+				out.push_back(s.at(i));
+				out.push_back('1');
+				out.push_back(')');
+				out.push_back('*');
+				i++;
+		}
+		else if((i == 0) && (i + 1 < s.length()) && (s.at(i) == '-') && (s.at(i+1) == '-') ) {
+			i = i + 2;
+		}
+		else if( (i >= 0) && (i-1 >= 0) && ((s.at(i) == '-')) && ((i + 2 < s.length()) && 
+			(s.at(i + 1) == '-') && (s.at(i-1) != '^')) ) {
+			if(s.at(i-1) != '(') {
+				out.push_back('+');
+			}
+			i = i+2;
+		}
+		//else if( (i + 1 < s.length()) && s.at(i) == '^' && s.at(i+1) == '-' ) {
+			
+		//}
+		// This is for cases like e() or 4(67)
+		// If the previous character is a number or special
+		if( (i > 0) && (s.at(i) == '(') && 
+			(isNumber(s.at(i-1)) || (s.at(i-1) == 'p') || (s.at(i-1) == 'e')) ) {
+			out.push_back('*');
+		}		
+		else if((i > 0) && (s.at(i-1) == ')') && (s.at(i) == '(')) {
+			out.push_back('*');
+		}
+		else if ((i > 0) && (s.at(i-1) == ')') && !isOperator(s.at(i)) && 
+			(s.at(i) != '(') && (s.at(i) != ')') && (s.at(i) != ':') ) {
+			out.push_back('*');
+		}
+			out.push_back(s.at(i));
+	}
+	std::cout << "Missing Operator Check\n" << out << endl << endl;
+	s = out;
+}
+
+// Method:		finalStringCleanUp(string in)
+// Description:	This methods clean up the final string by adding extra parentheses
+//				around the negative numbers to make ease the shunting yard method
+//				Example: if the user entered 2*-7
+void ShuntingYard::finalStringCleanUp() 
+{
+	string out = "";
+	int i = 0;
+	while (i < s.length())
+	{
+		if( (i == 1) && (s.at(i-1) == '-') && isNumber(s.at(i)) ) {
+				out = "";
+				out.push_back('(');
+				out.push_back('-');
+				out.push_back(s.at(i));
+				i++;
+				while( (i < s.length()) && isNumber(s.at(i)) ) {
+					out.push_back(s.at(i));
+					i++;
+				}
+				out.push_back(')');
+		}
+		else if( (i > 0) && (isOperator(s.at(i-1)) || isOpenParen(s.at(i-1))) &&
+			(s.at(i)== '-') ) {
+				out.push_back('(');
+				out.push_back(s.at(i));
+				i++;
+				while( (i < s.length()) && isNumber(s.at(i)) ) {
+					out.push_back(s.at(i));
+					i++;
+				}
+				out.push_back(')');
+		}
+		else {
+			out.push_back(s.at(i));
+			i++;
+		}
+	}
+	// If prarmeter is removed, uncommment
+	// s = out;
+	cout << "Final Clean Up Check\n" << out << endl << endl;
+	s = out;
+}
+
+
+// Method:		toVector()
+// Parameters:	None
+// Description:	This method convert the given string to a vector
+//				to make sure that every number is seperated correctly
+//				Ex: (25*89-100)^79
+// return:		None
+void ShuntingYard::toVector() 
+{
+	string temp = "";
+
+	int i = 0;
+	while(i < s.size()) {
+		if( isNumber(s.at(i)) ) {
+			string num = "";
+			// Form the number
+			while( (i < s.size()) && isNumber(s.at(i)) ) {
+				num.push_back(s.at(i));
+				i++;
+			}
+			// Add the number to the vector
+			expression.push_back(num);
+			
+		}
+		else if( isOperator(s.at(i)) ) {
+			string oper = "";
+			oper.push_back(s.at(i));
+			// Add temp to vector then add operator
+			expression.push_back(oper);
+			i++;
+		}
+		else if ( isOpenParen(s.at(i)) || isCloseParen(s.at(i)) ) {
+			string par = "";
+			par.push_back(s.at(i));
+			// Add parenthese to the vector
+			expression.push_back(par);
+			i++;
+		}
+		else if(s.at(i) == ':') {
+			/*
+			expression.push_back(temp);
+			temp = "";
+			*/
+			if ( (i-1 >= 0) && s.at(i-1) != 't' && s.at(i-1) != 'n' && s.at(i-1) != 'g' ) {
+				expression.push_back(":");
+			}
+			else {
+			}
+			i++;
+		}
+		// If it is not anything else
+		else {
+			bool log_ = false;
+			// It must be a letter in this case of a special number, or log, or root
+			while((i < s.length()) && (!isOperator(s.at(i)) && !log_ && 
+				!isCloseParen(s.at(i))  && !isOpenParen(s.at(i)) && s.at(i) != ':' && !isNumber(s.at(i))) ) {
+				// Form the special number
+				temp.push_back(s.at(i));
+				// In case that it is a log_
+				if(s.at(i) == '_') {
+					log_ = true;
+				}
+				i++;
+			}
+			// At this point, temp is either e or pi, rt, sqrt, ln, log, or log_
+			if( isSpecialNumber(temp) ) {
+				expression.push_back(temp);
+				temp = "";
+			}
+			else if( temp == "log_" ) {
+				expression.push_back(temp);
+				temp = "";
+			}
+			else if( temp == "rt" ) {
+				expression.push_back(temp);
+				temp = "";
+			}
+			else if( temp == "sqrt" ) {
+				expression.push_back("2");
+				expression.push_back("rt");
+			}
+			else if( (!expression.empty()) && (expression.back() == "ln") ) {
+				expression.pop_back();
+				expression.push_back("log_");
+				expression.push_back("e");
+			}
+			else if( (!expression.empty()) && (expression.back() == "log") ) {
+				expression.pop_back();
+				expression.push_back("log_");
+				expression.push_back("10");
+			}
+			else if( (!expression.empty()) && (expression.back() == "sqrt") ) {
+				expression.pop_back();
+				expression.push_back("2");
+				expression.push_back("rt");
+			}
+		}
+	}
+}
+
+
+
+// Method:		void reversePolish()
+// Parameters:  none
+// Description:	This method convert the a given object into a format that follows
+//				the rules of PEMDAS, and put it in a way that is acceptable to
+//				the computer
+// Return:		None
+void ShuntingYard::reversePolish()
+{
+	stack<string> operations;
+	bool logInStack = false;
+	bool removeLog = false;
+	bool removeRt = false;
+
+	// First make expressionVector a fitted vector to prevent furture headache
+	expression.shrink_to_fit();
+
+	int i = 0;
+	while(i < expression.size()){
+		// if it is an operator, Check precedence
+		if( isOperator(expression.at(i)) ) {
+			if( operations.empty() ) {
+				operations.push( expression.at(i) );
+			}
+			// if the last operator 
+			else if( (!operations.empty()) && isOpenParen(operations.top()) ) {
+				operations.push( expression.at(i) );
+			}
+			else if( (!operations.empty()) && operations.top() == "log_" && removeLog && expression.at(i) != "^") {
+				queue.push_back(operations.top());
+				operations.pop();
+				operations.push(expression.at(i));
+			}
+			else if( (!operations.empty()) && operations.top() == "rt" && expression.at(i) != "^") {
+				queue.push_back(operations.top());
+				operations.pop();
+				operations.push(expression.at(i));
+			}
+			else if( (!operations.empty()) && (precedence(operations.top()) > precedence(expression.at(i))) ) {
+				// Remove until operation top <= current operator or pararentheses
+				while( (!operations.empty()) && !isOpenParen(operations.top().at(0)) && 
+					((precedence(operations.top()) > precedence(expression.at(i)))) ) {
+						queue.push_back( operations.top() );
+						operations.pop();
+				}
+				// Now add the operator to operations stack
+				operations.push( expression.at(i) );
+			}
+			else {
+				// Add to operations
+				operations.push( expression.at(i) );
+
+			}
+			i++;
+		}
+		else if( (i >= 0) && (i < expression.size()) && expression.at(i) == "(" ) {
+			operations.push( expression.at(i) );
+			i++;
+		}
+		else if( (i > 0) && (i < expression.size()) && expression.at(i) == ")" ) {
+			// Loop as long as ( is not on the last element in operations
+			// or operations is not empty
+			while( (!operations.empty()) && (!isOpenParen(operations.top())) ) {
+				queue.push_back( operations.top() );
+				operations.pop();
+			}
+			i++;
+			// Remove the left parenthese
+			operations.pop();
+
+			// In case of rt, or logBase
+			if( !operations.empty() && operations.top() == "rt") {
+				// Move rt to queue
+				queue.push_back( operations.top() );
+				operations.pop();
+			}
+			else if( !operations.empty() && operations.top() == "log_" && removeLog ) {
+				queue.push_back( operations.top() );
+				operations.pop();
+			}
+		}
+		else if( (expression.at(i) == ":") ) {
+			// Remove until log is on top of stack
+			// Add to the operation stack
+			while( !operations.empty() && operations.top() != "log_" ) {
+				queue.push_back( operations.top() );
+				operations.pop();
+			}
+			if( !operations.empty() && operations.top() == "log_" ) {
+				removeLog = true;
+			}
+			else {
+				// Do absolutely nothing
+			}
+			i++;
+		}
+		else if( (expression.at(i) == "rt") ) {
+			// Add to the operation stack
+			operations.push(expression.at(i));
+			i++;
+		}
+		else if( (expression.at(i) == "log_") ) {
+			// Add to the operation stack
+			operations.push(expression.at(i));
+			i++;
+		}
+		else {
+			// In case of negative number put it as it is
+			// Example -2, -e, -pi
+			if( (i > 0) && !operations.empty() && expression.at(i-1) == "-" && 
+				isOpenParen(expression.at(i-2).at(0)) ) {
+					// String has to be concatenated
+					string  temp;
+					temp = operations.top() + expression.at(i);
+					operations.pop();
+					queue.push_back(temp);
+					i++;
+			}
+			else {
+				// It must be a number or a special number 
+				// Add to queue
+				queue.push_back(expression.at(i));
+				i++;
+			}
+		}
+	}
+	while( !operations.empty() ) {
+		queue.push_back( operations.top() );
+		operations.pop();
+	}
+}
+
+void ShuntingYard::calculate()
+{
+	// loop through the polish vector
+	for(int i = 0; i < queue.size(); i++) {
+
+	}
+}
+
+void ShuntingYard::printInfo()
+{
+	cout << "Expression in vector" << endl; 
+	for(int i = 0; i < expression.size(); i++) {
+		cout << expression.at(i) << "  ";
+	}
+
+	cout << endl;
+
+	
+	cout << "\nExpression in reversePolish vector" << endl; 
+	for(int i = 0; i < queue.size(); i++) {
+		cout << queue.at(i) << "  ";
+	}
+	
+}
---
+++ CalculatorProject/CalculatorProject/main.cpp
@@ -0,0 +1,235 @@
+#include<iostream>
+#include<stack>
+#include<vector>
+#include<string>
+#include<string.h>
+#include<stdio.h>
+#include "ShuntingYard.h"
+
+using namespace std;
+
+// Storage
+vector <string> inputExpression;	// To Store the input that user entered at first
+vector <string> expressionResult;	// To store an expression result
+vector <double> result;				// To store floating point value of user's answer
+
+// Prototypes
+int menu();
+bool ComputeNewOperation();
+void help();
+void historyMenu();
+
+
+int main() {
+	bool terminateProgram = false;
+	int menuChoice = 0;
+	
+	while(terminateProgram == false) {
+		menuChoice = menu();
+		cout << endl;
+		switch(menuChoice){
+		case 1: terminateProgram = ComputeNewOperation();
+			break;
+		case 2: historyMenu();
+			break;
+		case 3: help();
+			break;
+		case 0: terminateProgram = true;
+			break;
+		}// Close switch statement
+	}
+	cout << "\nGoodbye!" <<endl << endl; 
+	system("pause");
+	return 0;
+}
+
+// Function:	menu()
+// Parameters:	None
+// Description:	This function keep printing the menu to the user.
+//				until a valid choice is seleted. It also check for input faiures.
+//				it return the menu choice (an integer).
+// Return:		selection (Menu choice selected by the user)
+int menu() {
+	int selection = 0;
+	bool quit;
+	do {
+		cout << "***********************************************************" <<endl;
+		cout << "                       Calculator 2.0                      " <<endl;
+		cout << "-----------------------------------------------------------" <<endl;
+		cout << "                      << Main Menu >>                      " << endl;
+		cout << "1. Compute new expression"<<endl;
+		cout << "2. Review previous expressions" <<endl;
+		cout << "3. Help" <<endl;
+		cout << "4. Quit" <<endl <<endl;
+
+		cout << "Enter a menu choice: ";
+		cin >> selection;
+
+		if(cin.fail()) {
+			cin.clear();
+			cin.ignore(INT_MAX, '\n');
+			
+			cout << "Invalid input!!" <<endl;
+			cout << "Enter and integer\n" <<endl;
+			quit = false;
+		}
+		else if ((selection < 1) || (selection > 4)) {
+			cout << "Invalid Input!"<<endl;
+			cout << "Enter a choice (1-4)\n" <<endl;
+			quit = false;
+		}
+		else {
+			quit = true;
+		}
+	} while(!quit);
+	return selection;
+}
+
+// Function:	compute()
+// Parameter:	None
+// Description:	This function will do all the stuffs nececessary
+//				to get the answers in expression as well as in decimal
+//				value
+// return:		None
+bool ComputeNewOperation() {
+	
+	// Local variables
+	string input;
+	bool quit = false;
+	bool exitCompute = false;
+	bool firstRun = true;
+	
+	cout << "-----------------------------------------------------------" << endl;
+	cout << "                   <<   Calculate   >>                     " << endl << endl;
+
+	// To clean any last bad (\n) character in the cin
+	cin.ignore();
+
+	do {
+		// Create an object of the class Shunting yard
+		ShuntingYard* op = new ShuntingYard();
+
+		cout << "Type \"back\" to go back to Main Menu: " << endl;
+		cout << "Type \"quit\" to exit Calculator 2.0!: " << endl << endl;
+
+		cout << "Enter an expression: ";
+		getline(cin, input);
+
+		if(input == "quit") {
+			quit = true;
+			exitCompute = true;
+			// Delete the ShuntingYard object using the destructor
+			ShuntingYard();
+		}
+		else if(input == "back") {
+			// Do nothing because the user wish to go back
+			// to main menu
+			exitCompute = true;
+			// Delete the ShuntingYard object using the destructor
+			ShuntingYard();
+		}
+		else {
+			if(!input.empty()) {
+				// Call to the correponding operations goes inside
+				// this block
+				op->setString(input);
+				op->removeSpaces();
+				op->addMissingOPerator();
+				op->finalStringCleanUp();
+
+				op->toVector();
+				op->reversePolish();
+				op->printInfo();
+				
+
+				expressionResult.push_back("hah");
+				expressionResult.push_back("lmao");
+				expressionResult.push_back("seriously");
+
+				result.push_back(45);
+				result.push_back(75);
+				result.push_back(65);
+
+				// This is the  end of the block to the operations
+				cout << endl;
+				// Delete the ShuntingYard object using the destructor
+				ShuntingYard();
+			}
+			cout << endl;
+		}
+	} while( !exitCompute );
+
+	return quit;
+}
+
+// Function:	historyMenu()
+// Parameter:	None
+// Description:	This method works perfectly so far except it is missing the option 
+//				to ask user whether her or she needs decimal values
+// return:		none
+void historyMenu() {
+	char floating;
+	int in;
+	int hist;
+		cout << "-----------------------------------------------------------" << endl;
+		cout << "                    <<   History!   >>                     " << endl << endl;
+		cout << "Display floating value (Y or N): ";
+		cin >> floating;
+	
+		while( cin.fail() || (floating != 'y') && (floating != 'n') && (floating != 'Y') && (floating != 'N') ) {
+			cin.clear();
+			cin.ignore(INT_MAX, '\n');
+			cout << "Wrong Input!\n"<< endl;
+			cout << "Enter Y for yes and N for No: ";
+			cin >> floating;
+		}
+		
+
+		if( !inputExpression.empty() && (floating == 'y' || floating == 'Y') ) {
+			int print = 1;
+			int counter = inputExpression.size() - 1;
+			while( counter >= 0 ) {
+				cout << print << "\t" << inputExpression.at(counter) 
+					<<"\t\t"  << result.at(counter)<< endl;
+				counter--;
+				print++;
+			}
+		}
+		else if( !inputExpression.empty() && (floating == 'n' || floating == 'N') ) {
+			int print = 1;
+			int counter = inputExpression.size() - 1;
+			while( counter >= 0 ) {
+				cout << print << "\t" << inputExpression.at(counter) 
+					<<"\t\t"  << expressionResult.at(counter)<< endl;
+				counter--;
+				print++;
+			}
+		}
+		else {
+			cout << "No operations has been performed yet" << endl;
+			cout << "No history available yet" << endl << endl;
+		}
+}
+
+// Function:	void help()
+// Parameter:	None
+// Description:	This method print out the instruction to the user
+//				it also has a one option submenu which allow the user 
+//				to go back to the main menu once the user is done reading
+// Return		none
+void help() {
+	int in = 0;
+	cout << "-----------------------------------------------------------" << endl;
+	cout << "                    << Instructions >>                     " << endl << endl;
+	cout << "This page is under construction" << endl << endl;
+	cout << "Enter \"9\" to go back to the main menu:";
+	cin >> in;
+	while( cin.fail() || (in != 9) ) {
+		cin.clear();
+		cin.ignore(INT_MAX, '\n');
+		cout << "Wrong Input!\n"<< endl;
+		cout << "Enter the number \"9\" if you wish to go back to the Main Menu:";
+		cin >> in;
+	}
+	cout << endl;
+}
