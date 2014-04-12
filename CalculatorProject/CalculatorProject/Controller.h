e2acd1ef15fb681139d81e6a37714c1988960133
joekleespies <joeykleespies@gmail.com>
2014-04-12 15:21:59 -0400

    Update Controller.h

--- CalculatorProject/CalculatorProject/Controller.h
+++ CalculatorProject/CalculatorProject/Controller.h
@@ -4,14 +4,33 @@
 #include <string>
 #include <sstream>
 #include <vector>
-#include "Fraction.h"
+
+#include "Number.h"
+#include "Integer.h"
+#include "Rational.h"
+
+#include "SpecialCase.h"
+#include "NatE.h"
+#include "Pi.h"
+#include "Log.h"
+#include "Root.h"
+#include "Expression.h"
+
+#include "Operations.h"
+#include "Add.h"
+#include "Subtract.h"
+#include "Multiply.h"
+#include "Divide.h"
+#include "Exponentiate.h"
+#include "nRoot.h"
 
 using namespace std;
 
 class Controller{
 
 public:
-	vector<string> parseQueue(vector<string> input);
+	vector<Number*> parseQueue(vector<string> input);
+	vector<Number*> convertToNumberVector(vector<string> queue);
 
 };
 
