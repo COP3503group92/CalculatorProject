417258ec71adb6832f8b55ed35824cc2ed73e671
joekleespies <joeykleespies@gmail.com>
2014-04-12 16:33:30 -0400

    Update Controller.h

--- CalculatorProject/CalculatorProject/Controller.h
+++ CalculatorProject/CalculatorProject/Controller.h
@@ -1,6 +1,9 @@
 #ifndef CONTROLLER_H_
 #define CONTROLLER_H_
 
+#include <iostream>
+#include <stdexcept>
+#include <stack>
 #include <string>
 #include <sstream>
 #include <vector>
@@ -29,8 +32,47 @@ using namespace std;
 class Controller{
 
 public:
+	Controller();
+	~Controller(void);
+
+	// Parsing Methods ==================================================
 	vector<Number*> parseQueue(vector<string> input);
 	vector<Number*> convertToNumberVector(vector<string> queue);
+
+	// Shunting Yard Methods ============================================
+	void setString(string);
+	void setQueue(vector<string>);
+	string getS();
+	vector<string> getQueue();
+
+	void removeSpaces();
+	bool isNumber(char);
+	bool isSpecialNumber(string);
+
+	bool isOperator(char);
+	bool isOperator(string);
+	bool isOpenParen(char);
+	bool isOpenParen(string);
+	bool isCloseParen(char);
+	bool isCloseParen(string);
+
+	string operatorToString(char);
+	int precedence(string);
+	void addMissingOPerator();
+	void finalStringCleanUp();
+
+	void toVector();
+	void reversePolish();
+	void printInfo();
+
+	void calculate();
+
+private:
+	string s;
+	stack <string> operators;
+	vector <string> expression;
+	vector <string> queue;
+	vector <string> answer;
 
 };
 
