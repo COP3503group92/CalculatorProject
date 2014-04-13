8455e484571f64ae470fe6cc8df75386a1fb62b5
Mikeykem <michaelkemerer@gmail.com>
2014-04-13 15:43:53 -0400

    Update NatE.h

--- CalculatorProject/CalculatorProject/NatE.h
+++ CalculatorProject/CalculatorProject/NatE.h
@@ -8,10 +8,18 @@ class NatE :
 {
 public:
 	NatE();
+	NatE(bool negative);
 	~NatE();
 	string getType();
+	Number* getCoefficient();
+	Number* getExponent();
+	void setExponent(Number* ex);
+	void setCoefficient(Number* co);
+	void simplify();
 private:
 	bool negative;
+	Number* coefficient;
+	Number* exponent;
 	const string typeName = "NatE";
 };
 
