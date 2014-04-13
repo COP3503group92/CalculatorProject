b34651d66d77d237778b046574d4a92f2581d678
Mikeykem <michaelkemerer@gmail.com>
2014-04-13 15:40:03 -0400

    Update Add.h

--- CalculatorProject/CalculatorProject/Add.h
+++ CalculatorProject/CalculatorProject/Add.h
@@ -8,8 +8,8 @@ class Add : public Operations
     public:
         Add();
         virtual ~Add();
-        Number* evaluate();
-
+        Number* evaluate(Number* a, Number* b);
+    protected:
     private:
         string cl = "+";
 };
